9738740cd0c40dba8899bd84f4c439c02dd980ca
Arkadiusz Dutka <arcadio.dutka@hotmail.com>
2021-11-09 18:41:17 +0100

    Base sorting interface

---
+++ Algogator/Sorting/ISorting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algogator
+{
+    public interface ISorting<T>
+    {
+        public delegate void SortIteratedEventHandler(ISorting<T> sender, EventArgs e);
+        public event SortIteratedEventHandler SortIterated;
+        public T[] getArray();
+        void sort();
+    }
+}
